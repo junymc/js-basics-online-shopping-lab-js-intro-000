ef49a92e17c88575f751ce371fb3b943a039fe83
Junyoung McArdle <juny.mcardle@gmail.com>
2018-11-29 18:30:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,21 +10,57 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var price = parseInt(Math.random()*(100 - 1) + 1)
+ var shoppingItem = {itemName:item,itemPrice:price}
+  cart.push(shoppingItem)
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+    return "Your shopping cart is empty."
+  }
+ // var item = cart[0]
+ //  if (cart.length === 1) {
+ //    return `In your cart, you have ${item.itemName} at $${item.itemPrice}.`
+ //  }
+  // var item2 = cart[1]
+  // if (cart.length === 2) {
+  //   return `In your cart, you have ${item.itemName} at $${item.itemPrice}, and ${item2.itemName} at $${item2.itemPrice}.`
+  // }
+  var string = "In your cart, you have "
+  for (let i = 0; i < cart.length; i++) {
+   if (cart.length - 1 === i && i != 0) {
+     string = string.concat("and ")
+   }
+   var itemX = cart[i]
+     string = string.concat(`${itemX.itemName} at $${itemX.itemPrice}`)
+   if (cart.length - 1 != i) {
+     string = string.concat(", ")
+   } else {
+     string = string.concat(".")
+   }// If it's not the last item in the cart, add , and space
+   // else, add .
+ }
+  return string
 }
 
 function total() {
-  // write your code here
+  var totalPrice = 0
+  for(let i = 0; i < cart.length; i++) {
+  var itemX = cart[i]
+   totalPrice += itemX.itemPrice
+  }
+  return totalPrice
 }
 
-function removeFromCart(item) {
-  // write your code here
-}
-
-function placeOrder(cardNumber) {
-  // write your code here
-}
+// function removeFromCart(item) {
+//   if () {
+//     return "That item is not in your cart."
+//   }
+//
+// }
+//
+// function placeOrder(cardNumber) {
+//   // write your code here
+// }
